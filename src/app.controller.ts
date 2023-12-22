078ab43b4800fb38beadad3ee6f6eebd77afa560
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-12-22 22:23:32 +0100

    Enabled getting file names with exported data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs';
+import * as path from 'path';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
+  @Get('get-files')
+  async getFiles() {
+    try {
+      const dirPath = path.resolve(__dirname, '../export_data');
+      const files = fs.readdirSync(dirPath);
+
+      console.log('Uzyskano dostęp do danych.');
+
+      const fileNames = files.map((file) => {
+        const filePath = path.join(dirPath, file);
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+
+        return JSON.parse(fileContent).name;
+      });
+      
+      return fileNames;
+
+    } catch (err) {
+      console.error(`Błąd podczas odczytu zawartości folderu z danymi: ${err}.`);
+      return `Błąd: ${err}.`;
+    }
+  }
 }
