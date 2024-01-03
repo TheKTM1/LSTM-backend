2595666c2bc4903375ae35716c0945febd11bb9b
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2024-01-03 21:26:35 +0100

    Added the method for returning file content with the post method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Res, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,12 +8,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('get-files')
-  async getFiles() {
+  async getFiles(
+    @Res() res
+  ) {
     try {
       const dirPath = path.resolve(__dirname, '../export_data');
       const files = fs.readdirSync(dirPath);
-
-      console.log('Uzyskano dostęp do danych.');
 
       const fileNames = files.map((file) => {
         const filePath = path.join(dirPath, file);
@@ -21,8 +21,37 @@ export class AppController {
 
         return JSON.parse(fileContent).name;
       });
+
+      console.log('Uzyskano dostęp do danych.');
       
-      return fileNames;
+      res.json(fileNames);
+
+    } catch (err) {
+      console.error(`Błąd podczas odczytu zawartości folderu z danymi: ${err}.`);
+      return `Błąd: ${err}.`;
+    }
+  }
+
+  @Post('send-file')
+  async sendFile(
+    @Body('name') name: string,
+  ){
+    try {
+      const dirPath = path.resolve(__dirname, '../export_data');
+      const files = fs.readdirSync(dirPath);
+
+      const fileData = files.map((file) => {
+        const filePath = path.join(dirPath, file);
+        const fileContentJson = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+        return fileContentJson;
+      })
+
+      const foundFile = fileData.find(file => file.name === name);
+
+      console.log(`Pobrano dane z pliku ${foundFile.name}.json.`);
+
+      return foundFile;
 
     } catch (err) {
       console.error(`Błąd podczas odczytu zawartości folderu z danymi: ${err}.`);
